f231b5c65a239b7df05f43786fcc89afbcce7334
Solomon Nwante <solomonchuks202@gmail.com>
2025-05-28 21:04:38 +0100

    Add props to make component reuseable

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,23 +1,41 @@
+import React from "react";
 
-export const MovieCard = () => {
+interface MovieCardProps {
+  posterUrl: string;
+  title: string;
+  genres: string[];
+  releaseDate: string;
+  overview: string;
+}
+
+export const MovieCard: React.FC<MovieCardProps> = ({
+  posterUrl,
+  title,
+  genres,
+  releaseDate,
+  overview,
+}) => {
+
   return (
     <div className="bg-white rounded-md shadow-md overflow-hidden flex flex-col h-full max-w-[350px] hover:shadow-red-500 transition-all cursor-pointer ml-4 mt-4">
       <img
-        src={"https://core-faucet.surge.sh/images/bento_pro_20.jpg"}
+        src={posterUrl}
         alt={`Movie poster`}
         className="w-full h-48 object-cover p-4 rounded-3xl"
       />
       <div className="p-4 flex flex-col flex-grow">
-        <h3 className="text-lg font-semibold mb-2">The Funny Chuks</h3>
-        <p className="text-sm text-gray-500 mb-1">
-          Release Date: 2025
-        </p>
+        <h3 className="text-lg font-semibold mb-2">{title}</h3>
+        {genres && genres.length > 0 && (
           <p className="text-sm text-gray-500 mb-2">
-            Genre: Comedy
+            Genre: {genres.join(", ")}
           </p>
+        )}
+        <p className="text-sm text-gray-500 mb-1">
+          Release Date: {releaseDate}
+        </p>
 
         <p className="text-sm text-gray-900 flex-grow text-[1rem]">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          {overview}
         </p>
       </div>
     </div>
