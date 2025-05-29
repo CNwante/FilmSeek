1547a7c7e80457e235a4a9a1ce55dd01fd29387f
Solomon Nwante <solomonchuks202@gmail.com>
2025-05-29 20:02:26 +0100

    Refactor 'select' styles to remove redundance

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -1,21 +1,30 @@
+import clsx from "clsx";
+
 export const FilterPanel = () => {
+  const selectStyle = clsx(
+    "w-60",
+    "px-4",
+    "py-2",
+    "border",
+    "border-gray-300",
+    "rounded-md shadow-sm",
+    "text-sm",
+    "focus:outline-none",
+    "focus:ring-2",
+    "focus:ring-red-500"
+  );
+
   return (
     <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-6">
       {/*==== Genre Filter ====*/}
-      <select
-        className="w-60 px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-red-500"
-        defaultValue=""
-      >
+      <select className={selectStyle} defaultValue="">
         <option value="" disabled>
           Select Genre
         </option>
       </select>
 
       {/*==== Year Filter ====*/}
-      <select
-        className="w-60 px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-red-500"
-        defaultValue=""
-      >
+      <select className={selectStyle} defaultValue="">
         <option value="" disabled>
           Select Release Year
         </option>
