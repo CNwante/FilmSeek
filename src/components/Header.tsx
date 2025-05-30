0830f88d15c3b10a2fd7136f13f4278c2180983b
Solomon Nwante <solomonchuks202@gmail.com>
2025-05-30 09:50:19 +0100

    Fix texts responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import clsx from "clsx";
 
 export const Header: React.FC = () => {
+  const fontSizes = "text-xl sm:text-2xl md:text-3xl";
+
   return (
     <header className="flex items-center justify-between p-4 bg-white shadow-md">
       <div className="flex items-center">
         <img src="src/assets/logo.png" alt="FilmSeek logo" className="w-14" />
-        <span className="text-3xl font-bold text-red-600">FilmSeek</span>
+        <span className={clsx(fontSizes, "font-bold text-red-600")}>
+          FilmSeek
+        </span>
       </div>
       <nav className="space-x-4">
-        <span className="text-gray-600 hover:text-blue-500 cursor-pointer text-2xl">
+        <span
+          className={clsx(
+            fontSizes,
+            "text-gray-600 hover:text-blue-500 cursor-pointer"
+          )}
+        >
           Search Movies 🔍
         </span>
       </nav>
