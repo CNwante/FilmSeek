f3b9b4c2f4d02f2c5770efdf3c74c0860b5f6fe1
Solomon C. Nwante <58191795+CNwante@users.noreply.github.com>
2025-06-02 01:33:57 +0100

    Add loading spinner and error images to MovieGrid component (#20)
    * Create MovieGrid component
    
    * Export MovieGridProps interface
    
    * Add loading spinner and error images

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -4,12 +4,30 @@ import type { MovieCardProps } from "./MovieCard";
 
 export interface MovieGridProps {
   movies: MovieCardProps[];
+  loading: Boolean;
 }
 
-export const MovieGrid: React.FC<MovieGridProps> = ({ movies }) => {
+export const MovieGrid: React.FC<MovieGridProps> = ({ movies, loading }) => {
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-full w-screen mt-[25dvh]">
+        <img
+          src="src/assets/spinner.webp"
+          className="w-16 md:w-20 lg:w-24 bg-gray-200 rounded-full"
+          alt="Loading spinner"
+        />
+      </div>
+    );
+
   if (!movies || movies.length === 0) {
     return (
-      <div className="text-gray-500 text-center py-8">No movies found</div>
+      <div className="flex items-center justify-center h-full w-screen">
+        <img
+          src="src/assets/not_found.png"
+          className="bg-gray-200 rounded-full"
+          alt="Loading spinner"
+        />
+      </div>
     );
   }
 
